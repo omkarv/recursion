0ca0f36744b178786a0dbe4ec5365e8e130ca275
Omkar Vedpathak <omkarv@gmail.com>
2014-03-26 18:19:11 +0000

    Updated stringify JSON, passes tests.  Tidied up code.  Could be better.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,62 +4,78 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
-  //check if obj is an array, object
+  //check if input obj is a string, number, array, object, boolean or undefined
+  //then carry out resulting processing based on the input type 
   var outputString = "";
-  if(Array.isArray(obj)) {
+  if(Array.isArray(obj)) { // check if array
     outputString = outputString + "[";
     for (var item = 0; item < obj.length; item++) {
        var val = obj[item];
        if(Array.isArray(val)) {
-        outputString += stringifyJSON(obj[item]);
-       }  else if(typeof val === 'number') {
+        outputString += stringifyJSON(val); // recursive call of function, to obtain stringified array
+
+       } else if(typeof val === 'number') {
         outputString += obj[item];
+
        } else if((typeof val === 'string')||(typeof obj === 'boolean')) {
         outputString += '"' + obj[item] + '"';
+
        } else if(typeof val === 'undefined') {
         outputString += "null";
+
        } else if(typeof val === 'object') {
-        outputString += stringifyJSON(obj[item]);
+        outputString += stringifyJSON(obj[item]);  // recursive call of function, to obtain stringified object
        }
        //add comma between lines if item is not last in array
-       if(item !== (obj.length-1))
-       {
+       if(item !== (obj.length-1)) {
         outputString += ",";
        }
     }
     outputString = outputString + "]";
-    console.log(outputString);
-  } else if(typeof obj === 'object') {   
-      var count = 0;
-      
+
+  } else if(typeof obj === 'string') {    //check if string
+      outputString = '"' + obj + '"';
+
+  } else if(typeof obj === 'number') {   //check if number
+      outputString = obj.toString();
+
+  } else if((obj === null )||(typeof obj === 'boolean')) {    // check if null or boolean
+      outputString = "" + obj + "";
+
+  } else if(typeof obj === 'object') {         //check if object
+      var count = 0;  //counting no of processed key value pairs in current object
       outputString = outputString + "{";
       for (var key in obj) {
-             var noComma = false;
+             var noComma = false; //flag of whether to append a comma or not
              var value = obj[key];
              if(Array.isArray(value)) {
-              outputString +=  '"' + key + '"' + ":" + stringifyJSON(value);
-             } else if(typeof value === 'number') {
-              outputString += '"' + key + '"' + ":" + value; 
+
+              outputString +=  '"' + key + '"' + ":" + stringifyJSON(value);  // recur
+
+             } else if((typeof value === 'number')||(value === null)||(typeof value === 'boolean')) {
+              outputString += '"' + key + '"' + ":" + value;
+
              } else if(typeof value === 'string') {
               outputString += '"' + key + '"' + ":" + '"' + value + '"';
-             } else if(typeof value === 'boolean') {
-              outputString += '"' + key + '"' + ":" + value  ;
-             } else if(value === null) {
-              outputString +=  '"' + key + '"' + ":" + value; 
+
              } else if(typeof value === 'object') {
-              outputString +=  '"' + key + '"' + ":" + stringifyJSON(value); 
+              outputString +=  '"' + key + '"' + ":" + stringifyJSON(value);
+
              } else {//(typeof value === 'undefined') || (typeof obj === 'function')) {
-              //do nothing
-              noComma = true;
+              //do not append a comma when value is undefined or a function
+              noComma = true; // set do nothing flag
              }
-             // add comma between items if not last
-             count += 1;
-             if((count !== Object.keys(obj).length) && (!noComma)) // and 
-             {
+
+             // add comma between items if not last or the item is not to be outputted, as the value is fn or undefined
+             count += 1; // processed one key value pair in object, therefore increase count by one
+             if((count !== Object.keys(obj).length) && (!noComma)) {
               outputString += ",";
              }
       }
       outputString = outputString + "}";
+
+  } else if(obj === undefined) { //check if undefined
+    outputString = undefined;
 
   }
 
