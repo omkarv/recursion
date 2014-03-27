d60b732a5215abf77b322f077c187858a88f4fb9
Omkar Vedpathak <omkarv@gmail.com>
2014-03-27 11:22:38 +0000

    First rough implementation of getElementsByClassName.  Output needs to be flattened.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,32 @@
 // };
 
 // But in stead we're going to implement it from scratch:
+//can use documents.body, element.childNodes, element.classList.contains(classname)
 var getElementsByClassName = function (className) {
   // your code here
+  //iterate through classes in document.body.childNodes, checking whether they have the class in question
+  //by using the classList.contains(classname) method.  Each child Node childNode[...], may also have its own
+  //sub childNode, so its necessary to iterate through those too
+  
+  var checkChildNodes = function(selector) {
+    //if class is in childNode
+    // return an array of nodes which has the className in question
+    // or an object with numeric keys + length
+    if (selector.childNodes) { // if selector has child Nodes 
+      var childNodeSubElements = [];
+      _.each(selector.childNodes , function(element, index) {
+        if(selector.childNodes[index].classList){
+            if (selector.childNodes[index].classList.contains(className)) {
+              childNodeSubElements.push(element);
+            }
+            childNodeSubElements.push(checkChildNodes(selector.childNodes[index]));
+        }
+      });
+    return childNodeSubElements;
+    }
+  }
+
+  //return an array of nodes which have the className in question
+  return checkChildNodes(document.body);
+
 };
