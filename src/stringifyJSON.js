d8141ffe34353230758d5cfae8b9f83257291b9f
Omkar Vedpathak <omkarv@gmail.com>
2014-04-15 08:22:56 -0700

    object handling broken, array handling simplified

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,30 +8,12 @@ var stringifyJSON = function (obj) {
   //check if input obj is a string, number, array, object, boolean or undefined
   //then carry out resulting processing based on the input type 
   if(Array.isArray(obj)) { // check if array  ****** ARRAY HANDLING below
-    outputString = outputString + "[";
+    var results = [];
     for (var item = 0; item < obj.length; item++) {
        var val = obj[item];
-       if(Array.isArray(val)) {
-        outputString += stringifyJSON(val); // recursive call of function, to obtain stringified array
-
-       } else if(typeof val === 'number') {
-        outputString += obj[item];
-
-       } else if((typeof val === 'string')||(typeof obj === 'boolean')) {
-        outputString += '"' + obj[item] + '"';
-
-       } else if(typeof val === 'undefined') {
-        outputString += "null";
-
-       } else if(typeof val === 'object') {
-        outputString += stringifyJSON(obj[item]);  // recursive call of function, to obtain stringified object
-       }
-       //add comma between lines if item is not last in array
-       if(item !== (obj.length-1)) {
-        outputString += ",";
-       }
+       results.push(stringifyJSON(val));
     }
-    outputString = outputString + "]";
+    outputString = "[" + results.join(',') + "]";
 
   } else if(typeof obj === 'string') {    //check if string
       outputString = '"' + obj + '"';
@@ -40,42 +22,22 @@ var stringifyJSON = function (obj) {
       outputString = obj.toString();
 
   } else if((obj === null )||(typeof obj === 'boolean')) {    // check if null or boolean
-      outputString = "" + obj + "";
+      outputString = "" + obj;
+ 
+  } else if(obj === undefined) { //check if undefined
+    outputString = undefined;
 
   } else if(typeof obj === 'object') {         //check if object  ****** OBJECT HANDLING
       var count = 0;  //counting no of processed key value pairs in current object
-      outputString = outputString + "{";
+      //outputString = outputString + "{";
       for (var key in obj) {
              var noComma = false; //flag of whether to append a comma or not
              var value = obj[key];
-             if(Array.isArray(value)) {
-
-              outputString +=  '"' + key + '"' + ":" + stringifyJSON(value);  // recur
-
-             } else if((typeof value === 'number')||(value === null)||(typeof value === 'boolean')) {
-              outputString += '"' + key + '"' + ":" + value;
-
-             } else if(typeof value === 'string') {
-              outputString += '"' + key + '"' + ":" + '"' + value + '"';
-
-             } else if(typeof value === 'object') {
-              outputString +=  '"' + key + '"' + ":" + stringifyJSON(value);
-
-             } else {//(typeof value === 'undefined') || (typeof obj === 'function')) {
-              //do not append a comma when value is undefined or a function
-              noComma = true; // set do nothing flag
-             }
-
-             // add comma between items if not last or the item is not to be outputted, as the value is fn or undefined
-             count += 1; // processed one key value pair in object, therefore increase count by one
-             if((count !== Object.keys(obj).length) && (!noComma)) {
-              outputString += ",";
-             }
+             var output = [];
+             results.push(stringifyJSON(key) + ":" + stringifyJSON(value));
       }
-      outputString = outputString + "}";
+      outputString = "{" + results.join(",") + "}";
 
-  } else if(obj === undefined) { //check if undefined
-    outputString = undefined;
 
   }
 
